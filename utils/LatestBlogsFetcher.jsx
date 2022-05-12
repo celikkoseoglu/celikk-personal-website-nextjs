812cc03b0f4fd189d5d0d6210ef348802ebad608
Çelik Köseoğlu <celikkoseoglu@yahoo.com>
2022-05-12 21:36:12 +0100

    refactor LatestBlogsFetcher

--- utils/LatestBlogsFetcher.jsx
+++ utils/LatestBlogsFetcher.jsx
@@ -1,9 +1,5 @@
 export const NUMBER_OF_LATEST_BLOG_CARDS_TO_RENDER_ON_DESKTOP = 8;
 export const NUMBER_OF_LATEST_BLOG_CARDS_TO_RENDER_ON_MOBILE = 6;
 export const retrieveLatestBlogPosts = (allPosts) => {
-  const latestBlogsList = [];
-  for (let i = 0; i < NUMBER_OF_LATEST_BLOG_CARDS_TO_RENDER_ON_DESKTOP; i += 1) {
-    latestBlogsList.push(allPosts[i]);
-  }
-  return latestBlogsList;
+  return allPosts.slice(0, NUMBER_OF_LATEST_BLOG_CARDS_TO_RENDER_ON_DESKTOP);
 };
