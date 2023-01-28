ec2c9d1c59d1b803a9b16da40d3875a2c461327a
Çelik Köseoğlu <celikkoseoglu@yahoo.com>
2023-01-28 23:51:04 +0000

    replace next-compose-plugins with new solution

--- next.config.js
+++ next.config.js
@@ -1,34 +1,41 @@
-const withPlugins = require("next-compose-plugins");
 const optimizedImages = require("next-optimized-images");
 
-module.exports = withPlugins(
-  [
-    [
-      optimizedImages,
-      {
-        // config for next-optimized-images
-        optimizeImagesInDev: false,
-        handleImages: ["png", "webp"],
-        removeOriginalExtension: true,
+/* 
+ migrating from next-compose-plugins:
+ https://github.com/hashicorp/next-mdx-enhanced/issues/18#issuecomment-859167393
+ original source: https://dev.to/krzysztofzuraw/migrating-nextjs-plugins-from-next-compose-plugins-2gnl
+ */
+module.exports = () => {
+  const plugins = [optimizedImages /*, other plugins go here after the comma*/];
+  return plugins.reduce(
+    (acc, next) => {
+      if (next.name === "optimizedImages") {
+        return next(acc, {
+          // config for next-optimized-images
+          optimizeImagesInDev: false,
+          handleImages: ["png", "webp"],
+          removeOriginalExtension: true,
+        });
+      }
+
+      return next(acc);
+    },
+    {
+      images: {
+        disableStaticImages: true,
       },
-    ],
-    // your other plugins here
-  ],
-  {
-    images: {
-      disableStaticImages: true,
-    },
-    webpack(config, options) {
-      config.module.rules.push({
-        test: /\.(xml|pdf|ico|jpg|mp4|svg|txt|webmanifest)$/,
-        use: {
-          loader: "file-loader",
-          options: {
-            name: "[name].[hash].[ext]",
+      webpack(config, options) {
+        config.module.rules.push({
+          test: /\.(xml|pdf|ico|jpg|mp4|svg|txt|webmanifest)$/,
+          use: {
+            loader: "file-loader",
+            options: {
+              name: "[name].[hash].[ext]",
+            },
           },
-        },
-      });
-      return config;
-    },
-  }
-);
+        });
+        return config;
+      },
+    }
+  );
+};
